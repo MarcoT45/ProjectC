8a42af34401742bf98d4bdceba37468d316fffc3
MisterFufufu <misterfufufu@gmail.com>
2024-08-09 15:10:48 +0200

    Nouvelle police + Modifs Ecran titre & Hub + Changements trad Fr/En + Changement Livre Records

--- Assets/Scripts/Ecran Titre/QuitButtonController.cs
+++ Assets/Scripts/Ecran Titre/QuitButtonController.cs
@@ -12,12 +12,12 @@ public class QuitButtonController : MonoBehaviour {
     }
 
     public void OnButtonOver() {
-        this.text.fontSize = 140;
+        this.text.fontSize = 32;
         this.text.color = new Color(255, 0, 0, 255);
     }
 
     public void OnButtonExit() {
-        this.text.fontSize = 108;
+        this.text.fontSize = 16;
         this.text.color = new Color(255, 255, 255, 255);
     }
 
