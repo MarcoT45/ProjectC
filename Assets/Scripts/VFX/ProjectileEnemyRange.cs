92ce2f4446091dcd4ae5fbd80b40ee4b37fbe4b4
MisterFufufu <misterfufufu@gmail.com>
2025-11-26 17:41:33 +0100

    Petit équilibrage de l'ennemi range + Equilibrage magnetzone + Ajout d'un timer pour les détruire les projectiles qui ne touchent pas

--- Assets/Scripts/VFX/ProjectileEnemyRange.cs
+++ Assets/Scripts/VFX/ProjectileEnemyRange.cs
@@ -2,30 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileEnemyRange : MonoBehaviour
-{
+public class ProjectileEnemyRange : MonoBehaviour {
+
     public float speed = 5f;
+    public float timeRemaining = 5f;
     private Vector2 direction;
 
-    public void Initialize(Vector2 dir)
-    {
+    public void Initialize(Vector2 dir) {
         direction = dir.normalized;
     }
 
-    void Update()
-    {
+    private void FixedUpdate() {
         transform.position += (Vector3)direction * speed * Time.deltaTime;
+        TimerBeforeDestroy();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
             Destroy(gameObject);
-        }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
-        {
+        } else if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle")) {
             Destroy(gameObject); // Le projectile s’arrête sur un obstacle
         }
     }
+
+    // Si jamais on esquive, il faut détruire l'objet après un lapse de temps (5s) pour pas saturer la mémoire
+    private void TimerBeforeDestroy() {
+        if (timeRemaining > 0) {
+            timeRemaining -= Time.deltaTime;
+        } else {
+            Destroy(gameObject);
+        } 
+    }
+
 }
